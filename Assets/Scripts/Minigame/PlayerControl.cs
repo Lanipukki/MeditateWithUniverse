91758f1893cb84f398955b7bac08c63716de07aa
Lanipukki <49281068+Lanipukki@users.noreply.github.com>
2021-02-03 14:09:07 +0200

    scriptDone

---
+++ Assets/Scripts/Minigame/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControl : MonoBehaviour
+{
+
+
+    public float speed;
+    public float jump = 20f;
+
+    private Rigidbody rb;
+
+    bool isGrounded = true;
+
+    float forward;
+
+    void Start()
+    {
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+        void FixedUpdate()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddRelativeForce(movement * speed);
+        }
+         void Update()
+         {
+            bool player_jump = Input.GetButtonDown("Jump");
+            if (player_jump && isGrounded)
+            {
+                rb.AddForce(Vector3.up * jump);
+            }
+         }
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = false;
+            }
+        }
+    }
+}
