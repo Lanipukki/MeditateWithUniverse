61429259d6c804544e7c6ffdbe4f4fc0d15b14f6
Lanipukki <49281068+Lanipukki@users.noreply.github.com>
2021-02-05 12:21:57 +0200

    tässä koko paketti

--- Assets/Scripts/Minigame/BallPlayerController.cs
+++ Assets/Scripts/Minigame/BallPlayerController.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class BallPlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed;
+    private Rigidbody rb;
     void Start()
+
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+      rb.AddForce(movement * speed);
+        {
+            void OnTriggerEnter(Collider other)
+            {
+                if (other.gameObject.CompareTag("PickUp"))
+                {
+                    other.gameObject.SetActive(false);
+                }
+            }
+        }
+
         
     }
 }
